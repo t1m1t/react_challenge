0c9c60a4f63d771e689309f8c1de21f690a9283b
Scott Ninja Bowler <scott.bowler@learningfuze.com>
2017-04-18 09:53:09 -0700

    Created form using material-ui

--- src/components/app.js
+++ src/components/app.js
@@ -1,13 +1,22 @@
 import React from 'react';
+import RaisedButton from 'material-ui/RaisedButton';
+import TextField from 'material-ui/TextField';
+import Paper from 'material-ui/Paper';
+import Divider from 'material-ui/Divider';
 import styles from './app.css';
-import logo from './imgs/logo.svg';
 
 const App = () => (
-    <div>
-        <div className={styles.app}>
-            <img src={logo} className={styles.rotate}/>
-            <h1>Welcome to React!</h1>
-        </div>
+    <div className={styles.app}>
+        <Paper className={styles.addForm} zDepth={4}>
+            <div>
+                <h2>Add To Do Item</h2>
+                <Divider />
+                <TextField floatingLabelText="Enter An Item Title"/><br/>
+                <TextField floatingLabelText="Enter Item Details"/><br/>
+                <RaisedButton label="Add Item" primary={true}/>
+                <RaisedButton label="Reset Form" secondary={true}/>
+            </div>
+        </Paper>
     </div>
 );
 
--- src/index.js
+++ src/index.js
@@ -1,9 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import injectTapEventPlugin from 'react-tap-event-plugin'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import { blue500, cyan500 } from 'material-ui/styles/colors';
+
+injectTapEventPlugin();
 
 import App from './components/app';
 
+
+const muiTheme = getMuiTheme({
+    palette: {
+        accent1Color: cyan500,
+        primary1Color: blue500
+
+    },
+});
+
 ReactDOM.render(
-    <App />,
+    <MuiThemeProvider muiTheme={muiTheme}>
+        <App />
+    </MuiThemeProvider>,
     document.getElementById('root')
 );
