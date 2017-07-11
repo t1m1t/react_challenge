cb859789dee931baa92d29df4a4afe572840a2e9
Tim <timothy.yi7@gmail.com>
2017-07-11 16:49:05 -0700

    React intro examples

--- src/index.js
+++ src/index.js
@@ -3,7 +3,18 @@ import ReactDOM from 'react-dom';
 
 import App from './components/app';
 
+const user = {
+    firstName: 'Jim',
+    lastName: 'Bob'
+};
+
+function greet(user){
+    return user.firstName + ' ' + user.lastName;
+}
+
+const greeting = <h1>Hello, {greet(user)}</h1>;
+
 ReactDOM.render(
-    <App />,
+    greeting,
     document.getElementById('root')
 );
