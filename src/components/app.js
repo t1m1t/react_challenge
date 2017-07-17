b232a4181d4290fc74341e6b9c5dec505117dd40
Tim <timothy.yi7@gmail.com>
2017-07-17 10:45:49 -0700

    guessing game in react

--- src/components/app.js
+++ src/components/app.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import './app.css';
-import logo from './imgs/logo.svg';
+import './pick_number';
+import './make_guess';
+import pick_number from "./pick_number";
 
 const App = () => (
     <div>
         <div className="app">
             <img src={logo} className="rotate"/>
-            <h1>Welcome to React!</h1>
+            <h1>Guessing Game</h1>
         </div>
+        <pick_number />
     </div>
 );
 
