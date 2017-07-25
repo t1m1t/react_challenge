461f86854b6f7bba1ab0f7d24cdb35310cbf846a
Tim <timothy.yi7@gmail.com>
2017-07-24 17:06:30 -0700

    fresh fresh

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import './app.css';
+import logo from './imgs/logo.svg';
 
 
 const App = () => (
     <div>
         <div className="app">
-            <Greeting name="Kelly" color="blue" fruit="apple"/>
-            <Greeting name="Amber" color="hotpink" />
+            <img src={logo} className="rotate"/>
+            <h1>Welcome to React!</h1>
         </div>
     </div>
 );
