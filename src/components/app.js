3c7096b1f829b8edf5e2390ab8596a5b911a17a3
Tim <timothy.yi7@gmail.com>
2017-07-26 09:43:11 -0700

    continuation

--- src/components/app.js
+++ src/components/app.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import './app.css';
 import logo from './imgs/logo.svg';
-
+import Clock from './clock';
 
 const App = () => (
     <div>
         <div className="app">
             <img src={logo} className="rotate"/>
             <h1>Welcome to React!</h1>
+            <Clock />
         </div>
     </div>
 );
--- src/components/reducers/clock_reducer.js
+++ src/components/reducers/clock_reducer.js
@@ -1,7 +1,11 @@
+import types from '../actions/types';
+
 const DEFAULT_STATE = { currentTime: null };
 
 export default function (state = DEFAULT_STATE, action){
     switch (action.type){
+        case types.TICK:
+            return {...state, currentTime: new Date().toLocaleTimeString()};
         default:
             return state;
     }
