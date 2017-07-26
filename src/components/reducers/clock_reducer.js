51d99fbe5ea8a868bc03f1fc472fd1a731e2a26d
Tim <timothy.yi7@gmail.com>
2017-07-25 19:19:59 -0700

    practice

---
+++ src/components/reducers/clock_reducer.js
@@ -0,0 +1,8 @@
+const DEFAULT_STATE = { currentTime: null };
+
+export default function (state = DEFAULT_STATE, action){
+    switch (action.type){
+        default:
+            return state;
+    }
+}
