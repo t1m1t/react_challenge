1cb8e5704e3158c06e1fd445ad74a9668b2ab048
Tim <timothy.yi7@gmail.com>
2017-09-21 16:09:50 -0700

    added without transitions

--- src/components/app.js
+++ src/components/app.js
@@ -1,22 +1,189 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './app.css';
 import StaffSection from './staff_section';
-import PaneOne from './pane_1';
-import PaneTwo from './pane_2';
-import PaneThree from './pane_3';
+import homer from './imgs/homer_simpson.jpg';
+import lisa from './imgs/lisa_simpson.jpg';
+import abe from './imgs/abe_simpson.jpg';
+import artie from './imgs/arty.jpg';
+import ralph from './imgs/ralph.jpg';
+import patty from './imgs/patty.jpg';
 
 
-const App = () => (
-    <div className="about_us">
-        <div className="top_pane_area">
-            <PaneOne />
-            <PaneTwo />
-            <PaneThree />
-        </div>
-        <div className="bottom_staff_area">
-            <StaffSection />
-        </div>
-    </div>
-);
+class App extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            paneOne: {
+                profile_img: <img className="new_prof_pic" src={homer}/>,
+                name: 'Homer Simpson',
+                description: 'Homer is from the city of Springfield.'
+            },
+            paneTwo: {
+                profile_img: <img className="new_prof_pic" src={lisa}/>,
+                name: 'Lisa Simpson',
+                description: 'Lisa is from the city of Springfield.'
+            },
+            paneThree: {
+                profile_img: <img className="new_prof_pic" src={abe}/>,
+                name: 'Abe Simpson',
+                description: 'Abe is from the city of Springfield.'
+            }
+        }
+
+        this.image_clicked = this.image_clicked.bind(this);
+    }
+
+    image_clicked(source){
+        if (source == homer){
+            this.setState({
+                paneOne: {
+                    profile_img: <img className="new_prof_pic" src={homer}/>,
+                    name: 'Homer Simpson',
+                    description: 'Homer is from the city of Springfield.'
+                },
+                paneTwo:{
+                    profile_img: <span>{this.state.paneOne.profile_img}</span>,
+                    name: <span>{this.state.paneOne.name}</span>,
+                    description: <span>{this.state.paneOne.description}</span>
+                },
+                    paneThree:{
+                    profile_img: <span>{this.state.paneTwo.profile_img}</span>,
+                    name: <span>{this.state.paneTwo.name}</span>,
+                    description: <span>{this.state.paneThree.description}</span>
+                }
+            });
+        } else if (source == lisa){
+            this.setState({
+                paneOne: {
+                    profile_img: <img className="new_prof_pic" src={lisa}/>,
+                    name: 'Lisa Simpson',
+                    description: 'Lisa is from the city of Springfield.'
+                },
+                paneTwo:{
+                    profile_img: <span>{this.state.paneOne.profile_img}</span>,
+                    name: <span>{this.state.paneOne.name}</span>,
+                    description: <span>{this.state.paneOne.description}</span>
+                },
+                paneThree:{
+                    profile_img: <span>{this.state.paneTwo.profile_img}</span>,
+                    name: <span>{this.state.paneTwo.name}</span>,
+                    description: <span>{this.state.paneThree.description}</span>
+                }
+            });
+        } else if (source == abe){
+            this.setState({
+                paneOne: {
+                    profile_img: <img className="new_prof_pic" src={abe}/>,
+                    name: 'Abe Simpson',
+                    description: 'Abe is from the city of Springfield.'
+                },
+                paneTwo:{
+                    profile_img: <span>{this.state.paneOne.profile_img}</span>,
+                    name: <span>{this.state.paneOne.name}</span>,
+                    description: <span>{this.state.paneOne.description}</span>
+                },
+                paneThree:{
+                    profile_img: <span>{this.state.paneTwo.profile_img}</span>,
+                    name: <span>{this.state.paneTwo.name}</span>,
+                    description: <span>{this.state.paneThree.description}</span>
+                }
+            });
+        } else if (source == artie){
+            this.setState({
+                paneOne: {
+                    profile_img: <img className="new_prof_pic" src={artie}/>,
+                    name: 'Artie Ziff',
+                    description: 'Artie is from the city of Springfield.'
+                },
+                paneTwo:{
+                    profile_img: <span>{this.state.paneOne.profile_img}</span>,
+                    name: <span>{this.state.paneOne.name}</span>,
+                    description: <span>{this.state.paneOne.description}</span>
+                },
+                paneThree:{
+                    profile_img: <span>{this.state.paneTwo.profile_img}</span>,
+                    name: <span>{this.state.paneTwo.name}</span>,
+                    description: <span>{this.state.paneThree.description}</span>
+                }
+            });
+        } else if (source == ralph){
+            this.setState({
+                paneOne: {
+                    profile_img: <img className="new_prof_pic" src={ralph}/>,
+                    name: 'Ralph Wiggum',
+                    description: 'Ralph is from the city of Springfield.'
+                },
+                paneTwo:{
+                    profile_img: <span>{this.state.paneOne.profile_img}</span>,
+                    name: <span>{this.state.paneOne.name}</span>,
+                    description: <span>{this.state.paneOne.description}</span>
+                },
+                paneThree:{
+                    profile_img: <span>{this.state.paneTwo.profile_img}</span>,
+                    name: <span>{this.state.paneTwo.name}</span>,
+                    description: <span>{this.state.paneThree.description}</span>
+            }
+        });
+        }else if (source == patty){
+            this.setState({
+                paneOne: {
+                    profile_img: <img className="new_prof_pic" src={patty}/>,
+                    name: 'Patty Bouvier',
+                    description: 'Patty is from the city of Springfield.'
+                },
+                paneTwo:{
+                    profile_img: <span>{this.state.paneOne.profile_img}</span>,
+                    name: <span>{this.state.paneOne.name}</span>,
+                    description: <span>{this.state.paneOne.description}</span>
+                },
+                paneThree:{
+                    profile_img: <span>{this.state.paneTwo.profile_img}</span>,
+                    name: <span>{this.state.paneTwo.name}</span>,
+                    description: <span>{this.state.paneThree.description}</span>
+                }
+            });
+        }
+    }
+
+    render(){
+        return(
+            <div className="main_section">
+                <h1 className="about_us_title">About Us</h1>
+                <div className="top_pane_area">
+                    <div className="panes">
+                        <div className="profile_pane_pic">
+                            {this.state.paneOne.profile_img}
+                        </div>
+                        <div className="person_info">
+                            <h4>{this.state.paneOne.name}</h4>
+                            <h4>{this.state.paneOne.description}</h4>
+                        </div>
+                    </div>
+                    <div className="panes">
+                        <div className="profile_pane_pic">
+                            {this.state.paneTwo.profile_img}
+                        </div>
+                        <div className="person_info">
+                            <h4>{this.state.paneTwo.name}</h4>
+                            <h4>{this.state.paneTwo.description}</h4>
+                        </div>
+                    </div>
+                    <div className="panes">
+                        <div className="profile_pane_pic">
+                            {this.state.paneThree.profile_img}
+                        </div>
+                        <div className="person_info">
+                            <h4>{this.state.paneThree.name}</h4>
+                            <h4>{this.state.paneThree.description}</h4>
+                        </div>
+                    </div>
+                </div>
+                <div className="bottom_staff_area">
+                    <StaffSection image_clicked={this.image_clicked}/>
+                </div>
+            </div>
+        )
+    }
+};
 
 export default App;
--- src/components/staff_section.js
+++ src/components/staff_section.js
@@ -1,28 +1,26 @@
 import React, { Component } from 'react';
 import './app.css';
-import Homer from './imgs/homer_simpson.jpg';
-import Lisa from './imgs/lisa_simpson.jpg';
-import Abe from './imgs/abe_simpson.jpg';
-import Ned from './imgs/mini_ned.jpg';
-import Moe from './imgs/moe.jpg';
-import Patty from './imgs/patty.jpg';
+import homer from './imgs/homer_simpson.jpg';
+import lisa from './imgs/lisa_simpson.jpg';
+import abe from './imgs/abe_simpson.jpg';
+import artie from './imgs/arty.jpg';
+import ralph from './imgs/ralph.jpg';
+import patty from './imgs/patty.jpg';
 
 
 class StaffSection extends Component {
-    displayInfo(){
-        console.log('info displayed');
-    }
-
     render() {
+        let image_clicked = this.props.image_clicked;
+
         return(
             <div>
                 <div>
-                    <img onClick={this.displayInfo} className="prof_pic" src={Homer} />
-                    <img onClick={this.displayInfo} className="prof_pic" src={Lisa} />
-                    <img onClick={this.displayInfo} className="prof_pic" src={Abe} />
-                    <img onClick={this.displayInfo} className="prof_pic" src={Ned} />
-                    <img onClick={this.displayInfo} className="prof_pic" src={Moe} />
-                    <img onClick={this.displayInfo} className="prof_pic" src={Patty} />
+                    <img className="prof_pic" src={homer} onClick={() => image_clicked(homer)}/>
+                    <img className="prof_pic" src={lisa} onClick={() => image_clicked(lisa)}/>
+                    <img className="prof_pic" src={abe} onClick={() => image_clicked(abe)}/>
+                    <img className="prof_pic" src={artie} onClick={() => image_clicked(artie)}/>
+                    <img className="prof_pic" src={ralph} onClick={() => image_clicked(ralph)}/>
+                    <img className="prof_pic" src={patty} onClick={() => image_clicked(patty)}/>
                 </div>
             </div>
         )
